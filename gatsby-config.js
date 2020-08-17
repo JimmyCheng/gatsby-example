3a44232cc8965bf839f3eac7e7456a6bf51b7cea
Jimmy Cheng <jimmy@orchestrated.io>
2020-08-18 01:32:32 +1000

    Replace the index.js to index.mdx and add libs.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,5 @@
+const path = require("path")
+
 module.exports = {
   siteMetadata: {
     title: `My Personal Blog`,
@@ -5,6 +7,12 @@ module.exports = {
     author: `@gatsbyjs`,
   },
   plugins: [
+    {
+      resolve: `gatsby-plugin-mdx`,
+      options: {
+        defaultLayouts: { default: path.resolve("./src/components/layout.js") },
+      },
+    },
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-catch-links`,
     `gatsby-transformer-remark`,
