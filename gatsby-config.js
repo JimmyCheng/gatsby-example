d56d07b73221bb4f83ae99355b7ce7d0b3853a9d
Jimmy Cheng <jimmy@orchestrated.io>
2020-08-20 00:12:25 +1000

    Add plugin for gatsby-plugin-mdx.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,9 @@
 const path = require("path")
 const config = require("./config/website")
 
+const eggheadTransformer = require("./other/embedder-transformers/egghead")
+const here = (...p) => path.join(__dirname, ...p)
+
 require("dotenv").config({
   path: `.env.${process.env.NODE_ENV}`,
 })
@@ -48,6 +51,31 @@ module.exports = {
         defaultLayouts: {
           default: path.resolve("./src/components/layout_old.js"),
         },
+        extensions: [".mdx"],
+        gatsbyRemarkPlugins: [
+          { resolve: "gatsby-remark-copy-linked-files" },
+          {
+            resolve: "gatsby-remark-autolink-headers",
+            icon: `<svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd"><path d="M14.851 11.923c-.179-.641-.521-1.246-1.025-1.749-1.562-1.562-4.095-1.563-5.657 0l-4.998 4.998c-1.562 1.563-1.563 4.095 0 5.657 1.562 1.563 4.096 1.561 5.656 0l3.842-3.841.333.009c.404 0 .802-.04 1.189-.117l-4.657 4.656c-.975.976-2.255 1.464-3.535 1.464-1.28 0-2.56-.488-3.535-1.464-1.952-1.951-1.952-5.12 0-7.071l4.998-4.998c.975-.976 2.256-1.464 3.536-1.464 1.279 0 2.56.488 3.535 1.464.493.493.861 1.063 1.105 1.672l-.787.784zm-5.703.147c.178.643.521 1.25 1.026 1.756 1.562 1.563 4.096 1.561 5.656 0l4.999-4.998c1.563-1.562 1.563-4.095 0-5.657-1.562-1.562-4.095-1.563-5.657 0l-3.841 3.841-.333-.009c-.404 0-.802.04-1.189.117l4.656-4.656c.975-.976 2.256-1.464 3.536-1.464 1.279 0 2.56.488 3.535 1.464 1.951 1.951 1.951 5.119 0 7.071l-4.999 4.998c-.975.976-2.255 1.464-3.535 1.464-1.28 0-2.56-.488-3.535-1.464-.494-.495-.863-1.067-1.107-1.678l.788-.785z"/></svg>`,
+            enableCustomId: true,
+          },
+          {
+            resolve: "gatsby-remark-images",
+            options: {
+              backgroundColor: "#fafafa",
+              maxWidth: 1035,
+            },
+          },
+          {
+            resolve: `gatsby-remark-embedder`,
+            options: {
+              customTransformers: [eggheadTransformer],
+            },
+          },
+          {
+            resolve: require.resolve("./other/gatsby-remark-af"),
+          },
+        ],
       },
     },
     `gatsby-plugin-react-helmet`,
